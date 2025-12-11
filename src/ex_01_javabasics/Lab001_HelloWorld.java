937069f2f6b7114aa65b0c4e7e6177ec84cfb595
chandana <chandanak522001@gmail.com>
2025-12-11 17:08:42 +0530

    First Hello World Program

--- src/ex_01_javabasics/Lab001_HelloWorld.java
+++ src/ex_01_javabasics/Lab001_HelloWorld.java
@@ -1,7 +1,7 @@
 package ex_01_javabasics;
 
 public class Lab001_HelloWorld {
-    public static void main(String[]args)
+    public static void main(String[] args)
     {
         System.out.println("Hello World");
     }
