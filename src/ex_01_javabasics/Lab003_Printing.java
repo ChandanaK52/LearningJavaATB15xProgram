d25aade746c58e82feb876d100fae7b27c1c339f
chandana <chandanak522001@gmail.com>
2025-12-17 18:48:57 +0530

    Print,println,printf

---
+++ src/ex_01_javabasics/Lab003_Printing.java
@@ -0,0 +1,13 @@
+package ex_01_javabasics;
+
+public class Lab003_Printing {
+    static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.print("Welcome");
+        System.out.print("to learn java");
+        System.out.println(" ");
+        System.out.printf("Hi");
+        System.out.printf("Selenium");
+    }
+}
