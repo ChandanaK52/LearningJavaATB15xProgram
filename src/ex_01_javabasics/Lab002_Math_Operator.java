b0489382dc48953172cbf237ef5b2aee6c9b24fc
chandana <chandanak522001@gmail.com>
2025-12-17 18:41:16 +0530

    Mathematical operations

---
+++ src/ex_01_javabasics/Lab002_Math_Operator.java
@@ -0,0 +1,12 @@
+package ex_01_javabasics;
+
+public class Lab002_Math_Operator {
+    static void main(String[] args) {
+        System.out.println(2+7);
+        System.out.println("7-3");
+        System.out.println(95-35);
+        System.out.println(8*3);
+        System.out.println(40/20);
+
+    }
+}
